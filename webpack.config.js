a43a9e93a448f13d17100b83ccbed6e36df75c5d
Max <maxl.berkowitz@gmail.com>
2018-12-26 10:44:37 -0800

    mpn module cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,6 @@ module.exports = {
         exclude: [/node_modules/],
         use: [{ loader: 'babel-loader' }],
       },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [{ loader: 'file-loader', options: {} }],
-      },
     ],
   },
 };
