308357696030061824b2bea9cc5fd2710606b009
Max Berkowitz <maxl.berkowitz@gmail.com>
2019-01-11 10:46:27 -0800

    start script changes (#21)

--- startDigipotProgramming.bat
+++ startDigipotProgramming.bat
@@ -1,2 +1,11 @@
-start chrome /max --app=http://localhost:3000
-npm start
+@echo off
+goto again
+:again
+    set /p answer=Is the Digipot pluged into the computer? (Y/N)
+    if /i "%answer:~,1%" EQU "Y" goto run
+    if /i "%answer:~,1%" EQU "N" exit /b
+    echo Please type Y for Yes or N for No
+    goto again
+
+:run
+    start /min startProgram.bat
