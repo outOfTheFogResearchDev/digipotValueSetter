6620f6b192ce8c1e9cd1fb1649440f002731941e
Max <maxl.berkowitz@gmail.com>
2019-01-11 13:49:16 -0800

    start code question adjustment

--- startDigipotProgramming.bat
+++ startDigipotProgramming.bat
@@ -1,7 +1,7 @@
 @echo off
 goto again
 :again
-    set /p answer=Is the Digipot pluged into the computer? (Y/N)
+    set /p answer=Is the Digipot pluged into the computer (or are you just trying to print data)? (Y/N)
     if /i "%answer:~,1%" EQU "Y" goto run
     if /i "%answer:~,1%" EQU "N" exit /b
     echo Please type Y for Yes or N for No
