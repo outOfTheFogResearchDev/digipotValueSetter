5d13b6f261777d7fc083ad4f0b9686b6683a313d
Max <maxl.berkowitz@gmail.com>
2019-02-05 10:30:20 -0800

    added auto closing like a desktop application would

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { get } from 'axios';
 import App from './app/index';
+
+window.addEventListener('beforeunload', () => get('/exit'));
 
 render(<App />, document.getElementById('app'));
